5df30c8e87b8a046bd8826f256dc07ac3c0e90da
MathiasMK <mathiasmundtkofod@gmail.com>
2022-01-27 20:39:26 +0100

    End of first module on plurasight.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import {Component} from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'ab-root',
+
+  template: `
+    <div>
+      <h1>{{pageTitle}}</h1>
+      <div>My First Component</div>
+    </div>
+  `
 })
-export class AppComponent {
-  title = 'AngularBrushup';
+
+export class AppComponent{
+  pageTitle:string = "Mathias superstore";
 }
